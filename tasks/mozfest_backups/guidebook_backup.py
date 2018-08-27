a3b2da5d663b9d0a3c0466f5bab26d1f8dc4cc29
Lucie Daeye <lucied@mozillafoundation.org>
2018-08-27 17:44:52 +0200

    Upload files to s3

--- tasks/mozfest_backups/guidebook_backup.py
+++ tasks/mozfest_backups/guidebook_backup.py
@@ -1,12 +1,26 @@
 import json
 from datetime import datetime
-import requests
 import os
+
+import requests
+import boto3
 
 # GuideBook config
 API_URL = "https://builder.guidebook.com/open-api/v1/"
 API_KEY = os.environ["GUIDEBOOK_KEY"]
 GUIDE_ID = os.environ["GUIDE_ID"]
+
+# AWS configuration
+AWS_ACCESS_KEY_ID = os.environ["MOZFEST_AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["MOZFEST_AWS_SECRET_ACCESS_KEY"]
+S3_BUCKET = os.environ["MOZFEST_S3_BUCKET"]
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
 
 TIMESTAMP = datetime.now().strftime("%Y%m%d-%H%M")
 
@@ -34,13 +48,17 @@ def get_guidebook_content(guidebook_resource):
     return data
 
 
-# TODO: actually upload those files XD
 def upload_to_s3(guidebook_resource, json_content):
-    with open(f"{guidebook_resource}-{TIMESTAMP}.json", "w") as f:
-        json.dump(json_content, f, sort_keys=True, indent=4)
+    filename = f"{guidebook_resource}-{TIMESTAMP}.json"
+    data = json.dumps(json_content, sort_keys=True, indent=4).encode()
+    s3.put_object(Bucket=S3_BUCKET, Key=filename, Body=data)
+    print(f"Uploaded {guidebook_resource} to S3.")
 
 
-# Rollback: do a diff on what's actually on guidebook (redo a dump of everything) and last backup.
+# Alert: check for each file if timestamp is less than 3 hours
+# Cleanup back up: only keep 48h of data and wipe the rest.
+
+# Rollback (separate file + made to be executed locally): do a diff on what's actually on guidebook (redo a dump of everything) and last backup.
 # Check if tracks, location, session are different.
 # Do a intersection set with session IDs + diff
 # Check what's new (new ids)
