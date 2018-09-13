9be60bfbb81e66c3f8cc435ff8d659596d4a638e
Lucie Daeye <lucied@mozillafoundation.org>
2018-09-13 14:03:16 +0200

    Small cleanup backup script

--- tasks/mozfest_backups/guidebook_backup.py
+++ tasks/mozfest_backups/guidebook_backup.py
@@ -11,13 +11,27 @@ from utilities import (
 # put this in an env var
 guidebook_resources = ["guides", "sessions", "schedule-tracks", "locations"]
 
-# Get the bucket content and delete backups older than 2 days
-bucket_content = get_bucket_content()
-delete_old_backups(bucket_content)
 
-# For each Guidebook resource, check if the data is stale and upload new backups
-for resource in guidebook_resources:
-    filtered_content = filter_by_resources(bucket_content, resource)
-    is_stale(filtered_content)
-    guidebook_content = get_guidebook_content(resource)
-    upload_to_s3(resource, guidebook_content)
+def cleanup():
+    s3_previous_backups = get_bucket_content()
+
+    # Delete backups on S3 that are older than 2 days
+    delete_old_backups(s3_previous_backups)
+
+    # Alert if no backups were uploaded during the last 3 hours
+    for resource in guidebook_resources:
+        filtered_previous_backups = filter_by_resources(s3_previous_backups, resource)
+        is_stale(filtered_previous_backups)
+
+
+def do_backup():
+    for resource in guidebook_resources:
+        guidebook_data = get_guidebook_content(resource)
+        upload_to_s3(resource, guidebook_data)
+
+
+if __name__ == "__main__":
+    print("Starting backups of Guidebook data")
+    cleanup()
+    do_backup()
+    print("Backup complete")
