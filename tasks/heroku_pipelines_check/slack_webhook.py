d450006c3099ec921e7c38851127e915e63a0325
Lucie Daeye <lucied@mozillafoundation.org>
2018-10-15 14:35:52 +0200

    Add a slack webhook that post to a channel the list of commits that could be deployed to production.

---
+++ tasks/heroku_pipelines_check/slack_webhook.py
@@ -0,0 +1,52 @@
+import subprocess
+import requests
+import os
+
+pipelines = {
+    "foundation-site": "foundation-mofostaging-net",
+    "mozillafestival-org": "mozillafestival-org-staging",
+}
+
+slack_webhook = os.environ["SLACK_PIPELINES_WEBHOOK"]
+
+for app in pipelines:
+    output = (
+        subprocess.check_output(["heroku", "pipelines:diff", "-a", pipelines[app]])
+        .decode()
+        .splitlines(keepends=True)
+    )
+    title = output[1].strip("=\n").lstrip()
+
+    body = ''.join(output[4:-2])
+    diff_url = output[-1].strip("\n")
+
+    slack_payload = {
+        "attachments": [
+            {
+                "fallback": f"{app} can be promoted to production.\n"
+                f"URL to Github diff: {diff_url}\n"
+                f"URL to pipeline: https://dashboard.heroku.com/pipelines/{app}",
+                "pretext": f"{title}",
+                "title": f"Commit(s) to deploy to production:",
+                "text": f"{body}",
+                "color": "#36bced",
+                "actions": [
+                    {
+                        "type": "button",
+                        "text": "View Github diff",
+                        "url": f"{diff_url}",
+                    },
+                    {
+                        "type": "button",
+                        "text": "View pipeline on Heroku",
+                        "url": f"https://dashboard.heroku.com/pipelines/{app}",
+                    },
+                ],
+            }
+        ]
+    }
+
+    r = requests.post(f'{slack_webhook}',
+                      json=slack_payload,
+                      headers={'Content-Type': 'application/json'}
+                      )
