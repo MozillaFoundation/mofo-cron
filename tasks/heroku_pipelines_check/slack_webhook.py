d4ac308972c19893432b32489635d7a72d810c42
Lucie Daeye <lucied@mozillafoundation.org>
2018-10-15 15:43:54 +0200

    Don't post a message on slack if staging == prod

--- tasks/heroku_pipelines_check/slack_webhook.py
+++ tasks/heroku_pipelines_check/slack_webhook.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import requests
 import os
@@ -5,6 +6,7 @@ import os
 pipelines = {
     "foundation-site": "foundation-mofostaging-net",
     "mozillafestival-org": "mozillafestival-org-staging",
+    "donate-mozilla-org": "donate-mozilla-org-us-staging",
 }
 
 slack_webhook = os.environ["SLACK_PIPELINES_WEBHOOK"]
@@ -16,37 +18,42 @@ for app in pipelines:
         .splitlines(keepends=True)
     )
     title = output[1].strip("=\n").lstrip()
-
     body = ''.join(output[4:-2])
     diff_url = output[-1].strip("\n")
 
-    slack_payload = {
-        "attachments": [
-            {
-                "fallback": f"{app} can be promoted to production.\n"
-                f"URL to Github diff: {diff_url}\n"
-                f"URL to pipeline: https://dashboard.heroku.com/pipelines/{app}",
-                "pretext": f"{title}",
-                "title": f"Commit(s) to deploy to production:",
-                "text": f"{body}",
-                "color": "#36bced",
-                "actions": [
-                    {
-                        "type": "button",
-                        "text": "View Github diff",
-                        "url": f"{diff_url}",
-                    },
-                    {
-                        "type": "button",
-                        "text": "View pipeline on Heroku",
-                        "url": f"https://dashboard.heroku.com/pipelines/{app}",
-                    },
-                ],
-            }
-        ]
-    }
+    regex = "is up to date"
 
-    r = requests.post(f'{slack_webhook}',
-                      json=slack_payload,
-                      headers={'Content-Type': 'application/json'}
-                      )
+    if re.search(regex, title):
+        print(f"Nothing to promote for {app}")
+        pass
+    else:
+        slack_payload = {
+            "attachments": [
+                {
+                    "fallback": f"{app} can be promoted to production.\n"
+                    f"URL to Github diff: {diff_url}\n"
+                    f"URL to pipeline: https://dashboard.heroku.com/pipelines/{app}",
+                    "pretext": f"{title}",
+                    "title": f"Commit(s) to deploy to production:",
+                    "text": f"{body}",
+                    "color": "#36bced",
+                    "actions": [
+                        {
+                            "type": "button",
+                            "text": "View Github diff",
+                            "url": f"{diff_url}",
+                        },
+                        {
+                            "type": "button",
+                            "text": "View pipeline on Heroku",
+                            "url": f"https://dashboard.heroku.com/pipelines/{app}",
+                        },
+                    ],
+                }
+            ]
+        }
+
+        r = requests.post(f'{slack_webhook}',
+                          json=slack_payload,
+                          headers={'Content-Type': 'application/json'}
+                          )
