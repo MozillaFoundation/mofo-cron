72768d4419405df38ab878f25ce036c3e6b31360
Lucie Daeye <lucied@mozillafoundation.org>
2018-10-16 12:03:20 +0200

    Catch error + cleanup + add complete list of apps we want to track

--- tasks/heroku_pipelines_check/slack_webhook.py
+++ tasks/heroku_pipelines_check/slack_webhook.py
@@ -7,6 +7,8 @@ pipelines = {
     "foundation-site": "foundation-mofostaging-net",
     "mozillafestival-org": "mozillafestival-org-staging",
     "donate-mozilla-org": "donate-mozilla-org-us-staging",
+    "network-pulse": "network-pulse-staging",
+    "network-pulse-api": "network-pulse-api-staging",
 }
 
 slack_webhook = os.environ["SLACK_PIPELINES_WEBHOOK"]
@@ -17,43 +19,55 @@ for app in pipelines:
         .decode()
         .splitlines(keepends=True)
     )
-    title = output[1].strip("=\n").lstrip()
-    body = ''.join(output[4:-2])
-    diff_url = output[-1].strip("\n")
 
-    regex = "is up to date"
-
-    if re.search(regex, title):
-        print(f"Nothing to promote for {app}")
-        pass
+    if not output:
+        requests.post(
+            f"{slack_webhook}",
+            json={
+                "text": ":fire_engine: Error while running `slack_webhook.py` task on `mofo-cron`. Check logs at "
+                f"https://eu.logentries.com/app/3aae5f3f#/search/log/628eb861?last=Last%2020%20Minutes"
+            },
+            headers={"Content-Type": "application/json"},
+        ).raise_for_status()
+        break
     else:
-        slack_payload = {
-            "attachments": [
-                {
-                    "fallback": f"{app} can be promoted to production.\n"
-                    f"URL to Github diff: {diff_url}\n"
-                    f"URL to pipeline: https://dashboard.heroku.com/pipelines/{app}",
-                    "pretext": f"{title}",
-                    "title": f"Commit(s) to deploy to production:",
-                    "text": f"{body}",
-                    "color": "#36bced",
-                    "actions": [
-                        {
-                            "type": "button",
-                            "text": "View Github diff",
-                            "url": f"{diff_url}",
-                        },
-                        {
-                            "type": "button",
-                            "text": "View pipeline on Heroku",
-                            "url": f"https://dashboard.heroku.com/pipelines/{app}",
-                        },
-                    ],
-                }
-            ]
-        }
+        title = output[1].strip("=\n").lstrip()
+        body = "".join(output[4:-2])
+        diff_url = output[-1].rstrip()
+        up_to_date_regex = "is up to date"
 
-        r = requests.post(f'{slack_webhook}',
-                          json=slack_payload,
-                          headers={'Content-Type': 'application/json'}
-                          )
+        if re.search(up_to_date_regex, title):
+            print(f"Nothing to promote for {app}")
+        else:
+            print(f"Posting message to Slack for {app}.")
+            slack_payload = {
+                "attachments": [
+                    {
+                        "fallback": f"{app} can be promoted to production.\n"
+                        f"URL to Github diff: {diff_url}\n"
+                        f"URL to pipeline: https://dashboard.heroku.com/pipelines/{app}",
+                        "pretext": f"{title}",
+                        "title": f"Commit(s) to deploy to production:",
+                        "text": f"{body}",
+                        "color": "#36bced",
+                        "actions": [
+                            {
+                                "type": "button",
+                                "text": "View Github diff",
+                                "url": f"{diff_url}",
+                            },
+                            {
+                                "type": "button",
+                                "text": "View pipeline on Heroku",
+                                "url": f"https://dashboard.heroku.com/pipelines/{app}",
+                            },
+                        ],
+                    }
+                ]
+            }
+
+            requests.post(
+                f"{slack_webhook}",
+                json=slack_payload,
+                headers={"Content-Type": "application/json"},
+            ).raise_for_status()
